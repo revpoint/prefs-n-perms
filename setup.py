5c00785206f9063fc5e2ad77cb5925400d6c1d17
Ryan Anguiano <ryan.anguiano@gmail.com>
2015-06-22 22:31:38 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='prefs-n-perms',
+    version='0.1',
+    packages=['prefs_n_perms'],
+    include_package_data=True,
+    install_requires=[
+        'hiredis',
+        'redis',
+    ],
+    zip_safe=True,
+    url='https://github.com/revpoint/prefs-n-perms',
+    author='RevPoint Media LLC',
+    author_email='jangl@revpointmedia.com',
+    description='Manage preferences and permissions for Jangl app',
+)
