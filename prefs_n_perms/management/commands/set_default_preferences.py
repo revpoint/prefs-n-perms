623ca20d412ccc1f5fabb1edc18b36e2da73a9a8
Ryan Anguiano <ryan.anguiano@gmail.com>
2015-07-02 05:29:01 -0400

    Add reset default prefs command

---
+++ prefs_n_perms/management/commands/set_default_preferences.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from prefs_n_perms import section_registry
+
+
+class Command(BaseCommand):
+    help = 'Restore all default preferences to Redis DB.'
+
+    def handle(self, *args, **options):
+        for section, config in section_registry.iteritems():
+            self.stdout.write('Setting preferences for: %s' % section)
+            if config.default_preferences:
+                config.section.get_preferences().update_global(config.default_preferences)
+            if config.available_permissions:
+                config.section.get_permissions().add_available(config.available_permissions)
